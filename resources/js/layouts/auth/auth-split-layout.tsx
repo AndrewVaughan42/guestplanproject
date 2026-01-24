7fd6437084c3026a5bb71fe1b12a02c22888dab4
Andrew <andrewpatrickvaughan@gmail.com>
2026-01-24 15:12:40 +0000

    Venue image added to authentication layout.

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -3,6 +3,7 @@ import { home } from '@/routes';
 import { type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
+import loginImage from '../../pages/images/login-image.jpg';
 
 interface AuthLayoutProps {
     title?: string;
@@ -14,12 +15,17 @@ export default function AuthSplitLayout({
     title,
     description,
 }: PropsWithChildren<AuthLayoutProps>) {
-    const { name, quote } = usePage<SharedData>().props;
+    const { name } = usePage<SharedData>().props;
 
     return (
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
                 <div className="absolute inset-0 bg-zinc-900" />
+                <img
+                    src={loginImage}
+                    alt="Login image"
+                    className="absolute inset-0 h-full w-full object-cover opacity-20"
+                />
                 <Link
                     href={home()}
                     className="relative z-20 flex items-center text-lg font-medium"
@@ -27,18 +33,6 @@ export default function AuthSplitLayout({
                     <AppLogoIcon className="mr-2 size-8 fill-current text-white" />
                     {name}
                 </Link>
-                {quote && (
-                    <div className="relative z-20 mt-auto">
-                        <blockquote className="space-y-2">
-                            <p className="text-lg">
-                                &ldquo;{quote.message}&rdquo;
-                            </p>
-                            <footer className="text-sm text-neutral-300">
-                                {quote.author}
-                            </footer>
-                        </blockquote>
-                    </div>
-                )}
             </div>
             <div className="w-full lg:p-8">
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
