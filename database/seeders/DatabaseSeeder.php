564ceda00cf32bf2204e4d9458b730f95e59512f
Andrew <andrewpatrickvaughan@gmail.com>
2026-01-27 20:11:49 +0000

    Ammended test user to one set on browser for quick login after fresh seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::firstOrCreate(
-            ['email' => 'test@example.com'],
+            ['email' => 'andrew@laravel.com'],
             [
-                'name' => 'Test User',
-                'password' => 'password',
+                'name' => 'Andrew Vaughan',
+                'password' => 'qwertyuiop',
                 'email_verified_at' => now(),
             ]
         );
