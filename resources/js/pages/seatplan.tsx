4549ee8869c90418664c8c17637a0474df91c3b8
Andrew <andrewpatrickvaughan@gmail.com>
2026-02-08 22:55:44 +0000

    Canvas container border configured and stage size

--- resources/js/pages/seatplan.tsx
+++ resources/js/pages/seatplan.tsx
@@ -2,7 +2,8 @@ import { Head } from '@inertiajs/react'
 import { seatplan} from '@/routes';
 import AppLayout from '@/layouts/app-layout';
 import type { BreadcrumbItem } from '@/types';
-import { Layer, Rect, Stage } from 'react-konva';
+import { Circle, Layer, Stage } from 'react-konva';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Seatplan() {
     const breadcrumbs: BreadcrumbItem[] = [
@@ -11,28 +12,61 @@ export default function Seatplan() {
             href: seatplan.url(),
         },
     ];
-    //const canvasRef = useRef<HTMLCanvasElement>(null);
-    //const canvas = document.getElementById('seatplan');
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const [size, setSize] = useState({width: 0, height: 0});
+
+    const shapeSize = 100;
+    useEffect(() => {
+        if (!containerRef.current) return;
+        const observer = new ResizeObserver(([entry]) => {
+            const {width, height} = entry.contentRect;
+            setSize({width, height});
+        });
+
+        observer.observe(containerRef.current);
+        return () => observer.disconnect();
+    }, []);
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Seatplan"/>
+            <Head title="Seatplan" />
             <div className="flex h-full flex-1 flex-col gap-4 p-4">
-                <div className="flex-1 rounded-xl border border-sidebar-border/70 p-4 dark:border-sidebar-border">
-                    <Stage width={window.innerWidth} height={window.innerHeight}>
-                        <Layer>
-                            <Rect
-                                x={20}
-                                y={50}
-                                width={100}
-                                height={100}
-                                fill="red"
-                                shadowBlur={5}
+                <div
+                    className="flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
+                    style={{ overflow: 'hidden' }}
+                >
+                    <div className="h-full w-full p-4" ref={containerRef}>
+                        <Stage width={size.width} height={size.height}>
+                            <Layer>
+                                <Circle //Test Circle
+                                    x={20}
+                                    y={50}
+                                    radius={shapeSize / 2}
+                                    stroke={'white'}
+                                    draggable={true}
+                                    dragBoundFunc={(pos) => {
+                                        return {
+                                            x: Math.max(
+                                                0,
+                                                Math.min(
+                                                    pos.x,
+                                                    size.width - shapeSize,
+                                                ),
+                                            ),
+                                            y: Math.max(0,
+                                                Math.min(
+                                                    pos.y,
+                                                    size.height - shapeSize,
+                                                ),
+                                            ),
+                                        };
+                                    }}
                                 />
-                        </Layer>
-                    </Stage>
+                            </Layer>
+                        </Stage>
+                    </div>
                 </div>
             </div>
         </AppLayout>
-    )
+    );
 }
