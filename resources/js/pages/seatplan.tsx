b8986f5e660eb6540155670aa33ca0771426d0b3
Andrew <andrewpatrickvaughan@gmail.com>
2026-02-08 21:03:47 +0000

    React Konva Added. Seatplan page initial setup

--- resources/js/pages/seatplan.tsx
+++ resources/js/pages/seatplan.tsx
@@ -2,6 +2,7 @@ import { Head } from '@inertiajs/react'
 import { seatplan} from '@/routes';
 import AppLayout from '@/layouts/app-layout';
 import type { BreadcrumbItem } from '@/types';
+import { Layer, Rect, Stage } from 'react-konva';
 
 export default function Seatplan() {
     const breadcrumbs: BreadcrumbItem[] = [
@@ -10,15 +11,26 @@ export default function Seatplan() {
             href: seatplan.url(),
         },
     ];
+    //const canvasRef = useRef<HTMLCanvasElement>(null);
+    //const canvas = document.getElementById('seatplan');
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Seatplan"/>
             <div className="flex h-full flex-1 flex-col gap-4 p-4">
                 <div className="flex-1 rounded-xl border border-sidebar-border/70 p-4 dark:border-sidebar-border">
-                    <h2 className="mb-4 text-xl font-semibold">My Guests</h2>
-                    <p className="text-muted-foreground">
-                        This is where the seat planner will appear.
-                    </p>
+                    <Stage width={window.innerWidth} height={window.innerHeight}>
+                        <Layer>
+                            <Rect
+                                x={20}
+                                y={50}
+                                width={100}
+                                height={100}
+                                fill="red"
+                                shadowBlur={5}
+                                />
+                        </Layer>
+                    </Stage>
                 </div>
             </div>
         </AppLayout>
