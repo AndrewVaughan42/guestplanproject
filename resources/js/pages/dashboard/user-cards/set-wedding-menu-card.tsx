3fdd45a20f5b96d4cb166cbba947d8b8487de10e
Andrew <andrewpatrickvaughan@gmail.com>
2026-07-01 09:56:56 +0100

    Pie Chart Added to Wedding Menu Chart via recharts

--- resources/js/pages/dashboard/user-cards/set-wedding-menu-card.tsx
+++ resources/js/pages/dashboard/user-cards/set-wedding-menu-card.tsx
@@ -5,6 +5,7 @@ import { usePage } from '@inertiajs/react';
 import { useState } from 'react';
 import SetPartnerMeals from '@/pages/components/weddings/set-partner-meals';
 import { Button } from '@/components/ui/button';
+import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts';
 
 export default function SetWeddingMenuCard() {
     const { auth } = usePage<SharedData>().props;
@@ -29,107 +30,149 @@ export default function SetWeddingMenuCard() {
     const partnerB = wedding.guests.find((guest) => guest.role === 'partner_b');
     const hasPartners = !!partnerA?.menu_item_id && !!partnerB?.menu_item_id;
 
-    const statsMinThresh = (wedding.guest_count ?? 0) >= 10;
+    const statsMinThresh = (wedding.guests.length ?? 0) >= 10;
+
+    const graphColours = ['#007bff', '#28a745', '#dc3545'];
+
+    const mealData = wedding.menu_items?.map((item) => {
+        const count = wedding.guests.filter((guest) => guest.menu_item_id === item.id).length ?? 0;
+
+        return {
+            name: item.name,
+            value: count
+        }
+    })
 
     return (
         <>
             <DashCard
                 title={'Wedding Menu'}
                 content={
-                    <div className={'space-y-3'}>
-                        {!hasMenu ? (
-                            <>
-                                {/* No Menu */}
-                                <div
-                                    className={
-                                        'rounded-xl border border-dashed border-neutral-200 p-3 dark:border-neutral-800'
-                                    }
-                                >
-                                    <p className={'text-sm text-neutral-500'}>
-                                        No Menu Set Up Yet
-                                    </p>
-
-                                    <p className={'text-xs text-neutral-500'}>
-                                        Three dishes required, one of them
-                                        plant-based.
-                                    </p>
-
-                                    <Button
+                    <div className={'w-full max-w-5xl mx-auto px-2 sm:px-4 lg:px-8'}>
+                        <div className={"grid gap-4 lg:grid-cols-2"}>
+                            {!hasMenu ? (
+                                <>
+                                    {/* No Menu */}
+                                    <div
                                         className={
-                                            'mt-3 inline-flex items-center gap-2 rounded-lg bg-guestplan px-3 py-1.5 text-sm font-medium text-white hover:opacity-90'
+                                            'rounded-xl border border-dashed border-neutral-200 p-3 dark:border-neutral-800'
                                         }
-                                        onClick={() => setOpen(true)}
                                     >
-                                        Set Wedding Menu
-                                    </Button>
-                                </div>
-                            </>
-                        ) : (
-                            <>
-                                {/* Menu Set Up*/}
-                                <div>
-                                    <p>Wedding Menu Set Up</p>
-                                    <ul className="text-sm">
-                                        {wedding.menu_items?.map((item) => (
-                                            <div
-                                                key={item.id}
-                                                className={
-                                                    'flex items-center justify-between rounded-lg bg-neutral-50 px-3 py-2 dark:bg-neutral-900'
-                                                }
-                                            >
-                                                <span
-                                                    className={
-                                                        'text-sm font-medium'
-                                                    }
-                                                >
-                                                    {item.name}
-                                                </span>
+                                        <p className={'text-sm text-neutral-500'}>
+                                            No Menu Set Up Yet
+                                        </p>
 
-                                                {item.is_plant_based && (
-                                                    <span
+                                        <p className={'text-xs text-neutral-500'}>
+                                            Three dishes required, one of them
+                                            plant-based.
+                                        </p>
+
+                                        <Button
+                                            className={
+                                                'mt-3 inline-flex items-center gap-2 rounded-lg bg-guestplan px-3 py-1.5 text-sm font-medium text-white hover:opacity-90'
+                                            }
+                                            onClick={() => setOpen(true)}
+                                        >
+                                            Set Wedding Menu
+                                        </Button>
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    {/* Menu Set Up*/}
+                                    <div>
+                                        <div>
+                                            <p>Wedding Menu Set Up</p>
+                                            <ul className="text-sm">
+                                                {wedding.menu_items?.map((item) => (
+                                                    <div
+                                                        key={item.id}
                                                         className={
-                                                            'bg-green-500/20 px-1 py-2 text-xs font-medium text-green-400'
+                                                            'flex items-center justify-between rounded-lg bg-neutral-50 px-3 py-2 dark:bg-neutral-900'
                                                         }
                                                     >
-                                                        Plant Based
+                                                    <span
+                                                        className={
+                                                            'text-sm font-medium'
+                                                        }
+                                                    >
+                                                        {item.name}
                                                     </span>
-                                                )}
-                                            </div>
-                                        ))}
-                                    </ul>
-                                </div>
-                                {/* Sets Partner Menu Items*/}
 
-                                    <Button
-                                        className={
-                                            'mt-3 inline-flex items-center gap-2 rounded-lg bg-guestplan px-3 py-1.5 text-sm font-medium text-white hover:opacity-90'
-                                        }
-                                        onClick={() =>
-                                            setAddPartnerMealOpen(true)
-                                        }
-                                    >
-                                        {hasPartners ?  'Edit Your Dishes' : 'Set Your Dishes'}
-                                    </Button>
+                                                        {item.is_plant_based && (
+                                                            <span
+                                                                className={
+                                                                    'bg-green-500/20 px-1 py-2 text-xs font-medium text-green-400'
+                                                                }
+                                                            >
+                                                            Plant Based
+                                                        </span>
+                                                        )}
+                                                    </div>
+                                                ))}
+                                            </ul>
+                                        </div>
 
-
-                                {!statsMinThresh ? (
-                                    <p className={'text-xs text-neutral-500'}>
-                                        Wedding Statistics will show once you
-                                        have at least 10 guests.
-                                    </p>
-                                ) : (
-                                    <div className={'mt-3'}>
-                                        <p
+                                        {/* Sets Partner Menu Items*/}
+                                        <Button
                                             className={
-                                                'text-xs text-neutral-500'
+                                                'mt-3 inline-flex items-center gap-2 rounded-lg bg-guestplan px-3 py-1.5 text-sm font-medium text-white hover:opacity-90'
+                                            }
+                                            onClick={() =>
+                                                setAddPartnerMealOpen(true)
                                             }
                                         >
-                                            Meal Statistics
-                                        </p>
+                                            {hasPartners ?  'Edit Your Dishes' : 'Set Your Dishes'}
+                                        </Button>
                                     </div>
-                                )}
-                            </>
-                        )}
+
+
+                                    {!statsMinThresh ? (
+                                        <div className="flex items-center justify-center border border-dashed border-neutral-200 rounded-xl p-6">
+                                            <p className={'text-xs text-neutral-500'}>
+                                                Wedding Statistics will show once you
+                                                have at least 10 guests.
+                                            </p>
+                                        </div>
+                                    ) : (
+                                        <div className={'mt-3'}>
+                                            <p
+                                                className={
+                                                    'text-xs text-neutral-500'
+                                                }
+                                            >
+                                                Meal Statistics
+                                            </p>
+                                            <div className="mt-4 h-64 w-full">
+                                                <ResponsiveContainer width="100%" height="100%">
+                                                    <PieChart>
+                                                        <Pie
+                                                            data={mealData}
+                                                            dataKey="value"
+                                                            nameKey="name"
+                                                            outerRadius={80}
+                                                            label
+                                                        >
+                                                            {mealData!.map((_, index) => (
+                                                                <Cell
+                                                                    key={`cell-${index}`}
+                                                                    fill={graphColours[index % graphColours.length]}
+                                                                    className="stroke-2 stroke-guestplan small"
+                                                                />
+                                                            ))}
+                                                        </Pie>
+
+                                                        <Tooltip />
+                                                    </PieChart>
+                                                </ResponsiveContainer>
+                                            </div>
+
+                                        </div>
+                                    )}
+                                </>
+                            )}
+                        </div>
+
                     </div>
                 }
             />
